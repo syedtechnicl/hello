e2201a213ecc81f0e227dee40e4604759485df43
Syed <83526948+syed8867@users.noreply.github.com>
2022-12-13 16:26:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App=()=>{
+return(
+  <>
+  <h1>hello worlds</h1>
+  </>
+)
+}
+export default App;
---
+++ src/Search.jsx
@@ -0,0 +1,8 @@
+const Search=()=>{
+return(
+  <>
+    <h1>Hello world nnnnnnnnnnnnnnnnnnnnnn</h1>
+  </>
+)
+}
+export default Search;
